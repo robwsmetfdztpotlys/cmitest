694ceb52825cf2f55a6f906c60766f1329890bc6
robwsmetfdztpotlys <robwsmetfdztpotlys@miucce.com>
2021-01-09 17:14:17 +0100

    utworzono nowy program

---
+++ code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  int tablica[n];
+  cout << "Podaj liczbe n:  ";
+  cin >> n;
+  
+  cout << "Podaj n liczb:   ";
+  for(int i = 0; i < n; i++) {
+    cin >> tablica[i];
+  }
+  
+  cout << "Liczby od konca: ";
+  for(int i = n - 1; i >= 0; i--) {
+    cout << tablica[i] << ' ';
+  }
+  cout << endl;
+  
+  return 0;
+}
