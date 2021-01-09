57a74f5472159f3820d9d737d5f7d955fdf9173b
robwsmetfdztpotlys <robwsmetfdztpotlys@miucce.com>
2021-01-09 17:24:41 +0100

    prawdziwa poprawka

--- code.cpp
+++ code.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main() {
   int n;
-  int tablica[n];
   cout << "Podaj liczbe n:  ";
   cin >> n;
+  
+  int *tablica = new int[n];
   
   cout << "Podaj n liczb:   ";
   for(int i = 0; i < n; i++) {
@@ -17,6 +18,8 @@ int main() {
     cout << tablica[i] << ' ';
   }
   cout << endl;
+  
+  delete[] tablica;
   
   return 0;
 }
